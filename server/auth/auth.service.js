7014d1565749711e1420d1b16abe0afcf4aae5fd
Talha Awan <talhamanzoorawan@gmail.com>
2017-02-26 16:33:51 +0500

    modify authentication and authorization layers

--- server/auth/auth.service.js
+++ server/auth/auth.service.js
@@ -15,34 +15,44 @@ var validateJwt = expressJwt({
  */
 
 function isAuthenticated(){
+
+
     return compose()
     // Validate jwt
         .use(function(req, res, next) {
-            // allow access_token to be passed through query parameter as well
-            if(req.query && req.query.hasOwnProperty('access_token')) {
-                req.headers.authorization = 'Bearer '+req.query.access_token;
+            // // allow access_token to be passed through query parameter as well
+            // if(req.query && req.query.hasOwnProperty('access_token')) {
+            //     req.headers.authorization = 'Bearer '+req.query.access_token;
+            // }
+            // // IE11 forgets to set Authorization header sometimes. Pull from cookie instead.
+            // if(req.query && typeof req.headers.authorization === 'undefined') {
+            //     req.headers.authorization = 'Bearer '+req.cookies.token;
+            // }
+            // validateJwt(req, res, next);
+            console.log(req.user);
+            if(req.user){
+              return next(req.user);
             }
-            // IE11 forgets to set Authorization header sometimes. Pull from cookie instead.
-            if(req.query && typeof req.headers.authorization === 'undefined') {
-                req.headers.authorization = 'Bearer '+req.cookies.token;
+            else{
+              return res.status(401).end();
             }
-            validateJwt(req, res, next);
+
         })
         // Attach user to request
-        .use(function(req, res, next) {
-            User.findById(req.user._id).exec()
-                .then(function(user) {
-                    if(!user) {
-                        return res.status(401).end();
-                    }
-                    req.user = user;
-                    next();
-                    return null;
-                })
-                .catch(function(err){
-                    next(err)
-                });
-        });
+        // .use(function(req, res, next) {
+        //     User.findById(req.user._id).exec()
+        //         .then(function(user) {
+        //             if(!user) {
+        //                 return res.status(401).end();
+        //             }
+        //             req.user = user;
+        //             next();
+        //             return null;
+        //         })
+        //         .catch(function(err){
+        //             next(err)
+        //         });
+        // });
 }
 module.exports.isAuthenticated = isAuthenticated;
 
--- server/resource/passage/admin/index.js
+++ server/resource/passage/admin/index.js
@@ -4,11 +4,11 @@ const Router = require('express').Router;
 const controller = require('./passage.controller');
 // const validationSchema = require('./user.validation.schema');
 // const validator = require('../../utils/request.validator.js');
-// const auth = require('../../auth/auth.service');
+const auth = require('../../../auth/auth.service');
 
 const router = new Router();
 
-router.get('/', controller.index);
+router.get('/', auth.hasRole('admin'), controller.index);
 router.get('/show/:id', controller.show);
 router.delete('/:id', controller.destroy);
 router.put('/:id', controller.update);
