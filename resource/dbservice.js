86b7c1aefa7d1bf2511516b6809640da10949d9c
Talha Awan <talhamanzoorawan@gmail.com>
2017-04-08 00:08:10 +0500

    remove extra directory

--- resource/dbservice.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-(function () {
-    let dbHelper = require('./DbHelper');
-    module.exports = {
-        createModel: function (modelName, entityDef, indexObject, collection) {
-            return dbHelper.createModel(modelName, entityDef, indexObject, collection);
-        }
-    };
-}())
